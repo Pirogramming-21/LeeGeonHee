2ebfe87fcc267b43ff207135323b50a69c235327
leegh1025 <leegh1025@naver.com>
2024-07-03 01:33:00 +0900

    [JavaScript-handling Events]

--- Javascript 쇼핑몰 미니게임 과제/main.js
+++ Javascript 쇼핑몰 미니게임 과제/main.js
@@ -7,7 +7,6 @@ function loadItems() {
 
 function displayItems(items) {
     const container = document.querySelector('.items');
-    const html = items.map(item => createHTMLString(item));
     container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
@@ -19,10 +18,32 @@ function createHTMLString(item) {
     </li>
     `;
 }
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
+function onButtonClick(event, items) {
+        const dataset = event.target.dataset;
+        const key = dataset.key;
+        const value = dataset.value;
+
+        if (key == null || value == null) {
+            return;
+        }
+        const filtered = items.filter(item => item[key] === value);
+        displayItems(filtered);
+        
+} 
+
+
 loadItems()
     .then(items => {
     console.log(items)
     displayItems(items);
-  //  setEventListeners(items)
+    setEventListeners(items)
 })
 .catch(console.log);
