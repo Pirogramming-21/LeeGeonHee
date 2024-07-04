2611aa84f02ad38924e48626898b4115ab43c339
leegh1025 <leegh1025@naver.com>
2024-07-04 22:55:24 +0900

    [8단계]

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,35 +1,28 @@
 num = 0;
 
-while num < 31 :
+def brGame():
     while True :
         try :
             count = int(input('부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) :'))
             if count in [1,2,3]:
-                break
+                return count
             else:
                 print('1,2,3 중 하나를 입력하세요')
         except ValueError :
             print('정수를 입력하세요')
 
+
+while num < 31 :
+    count = brGame()
     for i in range(count):
         num = num + 1
-        print(f'playerA :{num}')
+        print(f'playerA : {num}')
         if num >= 31:
             print('playerB win!')
             break
-        
-
-
-    while True:
-        try:
-            count = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능 :'))
-            if count in [1,2,3]:
-                break
-            else:
-                print('1,2,3 중 하나를 입력하세요')
-        except ValueError :
-            print('정수를 입력하세요')
-
+    if num >= 31:
+        break
+    count = brGame()
     for i in range(count):
         num = num +1
         print(f'playerB : {num}')
