c5d2fcfb0dcf768a48aacadb3da0df4931560eb7
leegh1025 <leegh1025@naver.com>
2024-07-04 20:54:48 +0900

    [4단계]

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -8,3 +8,9 @@ while True :
             print('1,2,3 중 하나를 입력하세요')
     except ValueError :
         print('정수를 입력하세요')
+
+while True:
+    for i in range(count):
+        num = num + 1
+        print(f'playerA :{num}')
+    break
